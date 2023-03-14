12a2e703d867544fca99ccaf94af5a5d2c5068ab
Robin <robin@Robins-Macbook-Air.local>
2023-03-14 10:37:27 +0100

    clean up the code and modify the wordpool

--- game.js
+++ game.js
@@ -72,17 +72,6 @@ function updateQuote() {
   // if (quoteNo < quotes_array.length - 1) quoteNo++;
   // else quoteNo = 0;
 
-  // quote_text.textContent = null;
-  // current_quote = getRandomWord();
-
-  // // separate each character and make an element
-  // // out of each of them to individually style them
-  // current_quote.split("").forEach((char) => {
-  //   const charSpan = document.createElement("span");
-  //   charSpan.innerText = char;
-  //   quote_text.appendChild(charSpan);
-  // });
-
   quote_text.textContent = null;
   
   // Generate three random words and concatenate them
