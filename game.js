e9b2ba4b764548c2a3dfda063b8ffcdb00f65cfa
Epic RG <40289211+EpicRG@users.noreply.github.com>
2023-03-27 11:23:09 +0000

    cleaning up some code

--- game.js
+++ game.js
@@ -57,41 +57,24 @@ let isGameRunning = false;
 let isGameStarting = false;
 
 function updateQuote() {
-  // quote_text.textContent = null;
-  // current_quote = quotes_array[quoteNo];
-
-  // // separate each character and make an element
-  // // out of each of them to individually style them
-  // current_quote.split("").forEach((char) => {
-  //   const charSpan = document.createElement("span");
-  //   charSpan.innerText = char;
-  //   quote_text.appendChild(charSpan);
-  // });
-
-  // // roll over to the first quote
-  // if (quoteNo < quotes_array.length - 1) quoteNo++;
-  // else quoteNo = 0;
 
   quote_text.textContent = null;
   
-  // Generate three random words and concatenate them
   let quoteWords = [];
   for (let i = 0; i < 5; i++) {
     quoteWords.push(getRandomWord());
   }
   current_quote = quoteWords.join(" ");
+
+  const lastCharSpan = document.createElement("span");
+  lastCharSpan.innerText = " ";
+  quote_text.appendChild(lastCharSpan);
   
-  // separate each character and make an element
-  // out of each of them to individually style them
   current_quote.split("").forEach((char) => {
     const charSpan = document.createElement("span");
     charSpan.innerText = char;
     quote_text.appendChild(charSpan);
   });
-
-  const lastCharSpan = document.createElement("span");
-  lastCharSpan.innerText = " ";
-  quote_text.appendChild(lastCharSpan);
 
   // roll over to the first quote
   if (quoteNo < quotes_array.length - 1) quoteNo++;
@@ -99,11 +82,9 @@ function updateQuote() {
 }
 
 function processCurrentText() {
-  // get current input text and split it
   curr_input = input_area.value;
   curr_input_array = curr_input.split("");
 
-  // increment total characters typed
   characterTyped++;
 
   errors = 0;
@@ -112,43 +93,33 @@ function processCurrentText() {
   quoteSpanArray.forEach((char, index) => {
     let typedChar = curr_input_array[index];
 
-    // character not currently typed
     if (typedChar == null) {
       char.classList.remove("correct_char");
       char.classList.remove("incorrect_char");
 
-      // correct character
     } else if (typedChar === char.innerText) {
       char.classList.add("correct_char");
       char.classList.remove("incorrect_char");
 
-      // incorrect character
     } else {
       char.classList.add("incorrect_char");
       char.classList.remove("correct_char");
 
-      // increment number of errors
       errors++;
     }
   });
 
-  // display the number of errors
   error_text.textContent = total_errors + errors;
 
-  // update accuracy text
   let correctCharacters = characterTyped - (total_errors + errors);
   let accuracyVal = (correctCharacters / characterTyped) * 100;
   accuracy_text.textContent = Math.round(accuracyVal);
 
-  // if current text is completely typed
-  // irrespective of errors
   if (curr_input.length == current_quote.length) {
     updateQuote();
 
-    // update total errors
     total_errors += errors;
 
-    // clear the input area
     input_area.value = "";
   }
 }
@@ -158,7 +129,6 @@ function startGame() {
   updateQuote();
   isGameRunning = true;
   
-  // clear old and start a new timer
   clearInterval(timer);
   timer = setInterval(updateTimer, 1000);
 
@@ -188,42 +158,32 @@ function resetValues() {
 
 function updateTimer() {
   if (timeLeft > 0) {
-    // decrease the current time left
     timeLeft--;
 
-    // increase the time elapsed
     timeElapsed++;
 
-    // update the timer text
     timer_text.textContent = timeLeft + "s";
   } else {
-    // finish the game
     finishGame();
   }
 }
 
 function finishGame() {
-  // stop the timer
+
   clearInterval(timer);
 
-  // disable the input area
   input_area.disabled = true;
 
-  // show finishing text
   quote_text.textContent = "Click on restart to start a new game.";
 
-  // display restart button
   restart_btn.style.display = "block";
 
-  // calculate cpm and wpm
   cpm = Math.round((characterTyped / timeElapsed) * 60);
   wpm = Math.round((characterTyped / 5 / timeElapsed) * 60);
 
-  // update cpm and wpm text
   cpm_text.textContent = cpm;
   wpm_text.textContent = wpm;
 
-  // display the cpm and wpm
   cpm_group.style.display = "block";
   wpm_group.style.display = "block";
 
